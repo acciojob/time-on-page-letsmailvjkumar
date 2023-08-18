d085c68f57bf938e67ed7b0b6847308f31c19d02
letsmailvjkumar <letsmailvjkumar@gmail.com>
2023-08-18 09:59:15 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(time => time + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>You've been on this page for {time} seconds.</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
